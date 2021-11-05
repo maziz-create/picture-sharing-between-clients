e48a0c6516eb7ab60481102621d030f9c2f61f3c
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-05 14:47:18 +0300

    Client | Added UploadImage Component in components folder. Added react-image-uploading default typescript codes into it. Added module css for all buttons styling.

---
+++ client/src/components/UploadImage/index.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import styles from '../styles.module.css'
+
+import ImageUploading, { ImageListType } from "react-images-uploading";
+
+const UploadImage = () => {
+    const [images, setImages] = useState([]);
+    const maxNumber = 69;
+
+    const onChange = (
+        imageList: ImageListType,
+    ) => {
+        setImages(imageList as never[]);
+    };
+
+    return (
+        <div>
+            <h2>Gönderilecek Resimler</h2>
+            <ImageUploading
+                multiple
+                value={images}
+                onChange={onChange}
+                maxNumber={maxNumber}
+            >
+                {({
+                    imageList,
+                    onImageUpload,
+                    onImageRemoveAll,
+                    onImageUpdate,
+                    onImageRemove,
+                    isDragging,
+                    dragProps
+                }) => (
+                    <div className="upload__image-wrapper">
+                        <button
+                            className={styles.buttonType1}
+                            style={isDragging ? { backgroundColor: "black", color: "#fff" } : undefined}
+                            onClick={onImageUpload}
+                            {...dragProps}
+                        >
+                            Tıkla veya Sürükle
+                        </button>
+                        <button
+                            className={styles.buttonType2}
+                            onClick={onImageRemoveAll}
+                        >
+                            Bütün Resimleri Sil
+                        </button>
+                        {imageList.map((image, index) => (
+                            <div key={index} className="image-item">
+                                <img src={image.dataURL} alt="" width="100" />
+                                <div className="image-item__btn-wrapper">
+                                    <button
+                                        className={styles.buttonType3}
+                                        onClick={() => onImageUpdate(index)}
+                                    >
+                                        Güncelle
+                                    </button>
+                                    <button
+                                        className={styles.buttonType3}
+                                        onClick={() => onImageRemove(index)}
+                                    >
+                                        Sil
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </ImageUploading>
+        </div>
+    )
+}
+
+export default UploadImage
