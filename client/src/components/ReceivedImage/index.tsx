8689c76a9fc4d277f554ceefabbf9813aac454c3
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-05 19:05:37 +0300

    Client | If there is no image from received other clients, defaultImage received from App.tsx will be showing. But not added functionality yet.

--- client/src/components/ReceivedImage/index.tsx
+++ client/src/components/ReceivedImage/index.tsx
@@ -1,7 +1,24 @@
-const ReceivedImage = () => {
+import './styles.css'
+import styles from '../styles.module.css'
+
+/*
+Socket servere ilk bağlandığımızda alacağımız default image'i App.tsx'den
+prop olarak alabilmek ve Typescript'in tip kısıtlamasına da takılmamak için bu Entity'i ürettik. 
+*/
+interface defaultImage {
+    path: string;
+}
+
+const ReceivedImage: React.FC<defaultImage> = (defaultImage) => {
     return (
         <div>
             <h2>Alınacak Resimler</h2>
+            <div className="received-images">
+                <button className={styles.buttonType2}>Bütün Resimleri Sil</button>
+                <div className="image-item">
+                    <img src={defaultImage.path} />
+                </div>
+            </div>
         </div>
     )
 }
