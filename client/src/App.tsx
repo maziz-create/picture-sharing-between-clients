d679c7352596233e8f3d71131b03c97f6aa1ce80
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-05 18:49:58 +0300

    Client | Integrated init() and subscribe() functions for socket server connection. Then Added defaultImage state with useState hook for receiving defaultImage path data from socket server. Then added useEffect hook for connectioning to socket server(with init) and subscribing to socket server(for default image). Then received data is passed into UploadImage and ReceivedImage Components.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+
+//socket server connection methods
+import { init, subscribe } from './socketApi'
 
 //components
 import UploadImage from './components/UploadImage'
 import ReceivedImage from './components/ReceivedImage'
 
 const App = () => {
+  const [defaultImage, setDefaultImage] = useState('');
+
+  useEffect(() => {
+    init(); //component yerleşmişse servere girebiliriz.
+    subscribe((defaultImage: string) => {
+      setDefaultImage(defaultImage);
+    }); //aboneliğimizi başlattık.
+  }, [])
+
   return (
     <div className="App">
-      <UploadImage />
-      <ReceivedImage />
+      <UploadImage path={defaultImage} />
+      <ReceivedImage path={defaultImage} />
     </div>
   );
 }
