c93bbb37e11e6d9a7087281a265da3dd8f02ef07
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-06 13:27:41 +0300

    Client | Received images array data from UploadImage Component for sending to socket server. Imported ImageListType for defining prop types (images).

--- client/src/components/SendImageButton/index.tsx
+++ client/src/components/SendImageButton/index.tsx
@@ -1,10 +1,32 @@
 import './styles.css'
 import styles from '../styles.module.css'
 
-const SendImageButton = () => {
+//socket methods
+import { send } from '../../socketApi'
+
+//alacağım propun tipini belirtmek için.
+import { ImageListType } from "react-images-uploading";
+
+interface Images {
+    images: ImageListType,
+}
+
+const SendImageButton: React.FC<Images> = (images) => {
     return (
         <div>
-            <button className={`send-button ${styles.buttonType3}`}>Gönder</button>
+            <button
+                onClick={() => {
+                    /*
+                    images alanını burada ben eklediğim için images.images olarak gönderiyorum.
+                    gönderilen data ImageListType türünde olduğu için gönderilen data
+                    aldığım prop'un images.images kısmına eklendi.
+                    */
+                    send(images.images);
+                }}
+                className={`send-button ${styles.buttonType3}`}
+            >
+                Gönder
+            </button>
         </div>
     )
 }
