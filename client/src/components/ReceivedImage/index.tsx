f4c79f9d1955bf92e979133a8c59823fdc9cd778
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-07 10:01:48 +0300

    Client | Image list datas from getting socket server are shown with map function. If there is no image list, default image is shown.

--- client/src/components/ReceivedImage/index.tsx
+++ client/src/components/ReceivedImage/index.tsx
@@ -1,5 +1,13 @@
+import { useState, useEffect } from 'react'
+
 import './styles.css'
 import styles from '../styles.module.css'
+
+//socket methods
+import { receiveImage } from '../../socketApi'
+
+//socketten gelecek olan imageList'in tipini belirtmek için.
+import { ImageListType } from "react-images-uploading";
 
 /*
 Socket servere ilk bağlandığımızda alacağımız default image'i App.tsx'den
@@ -10,14 +18,32 @@ interface defaultImage {
 }
 
 const ReceivedImage: React.FC<defaultImage> = (defaultImage) => {
+    const [imageList, setImageList] = useState<ImageListType>([]);
+
+    useEffect(() => {
+        receiveImage((imageListFromSocket: ImageListType) => {
+            setImageList(imageListFromSocket);
+        });
+    }, [])
+
     return (
         <div>
             <h2>Alınacak Resimler</h2>
             <div className="received-images">
                 <button className={styles.buttonType2}>Bütün Resimleri Sil</button>
-                <div className="image-item">
-                    <img src={defaultImage.path} />
-                </div>
+                {
+                    !imageList[0]
+                        ? <div className="image-item"><img src={defaultImage.path} alt="default" /></div>
+                        : <div></div>
+                }
+
+                {
+                    imageList[0] &&
+                    imageList.map((image, index) => (
+                        <div key={index} className="image-item">
+                            <img src={image.dataURL} alt="" width="100" />
+                        </div>
+                    ))}
             </div>
         </div>
     )
